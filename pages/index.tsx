029b21b389ec5862be84373869bfa8bd504ad6f6
amooryjubran <omarsalehjubran@gmail.com>
2023-04-23 12:44:54 -0400

    Yaaay

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { Fragment } from "react";
+
+export default function Home() {
+  return (
+    <Fragment>
+      <h1 className="text-2xl text-green-500">Hello World</h1>
+    </Fragment>
+  );
+}
