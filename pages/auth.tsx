b6483bbb4af4ea8d3cddd2732ef8cf1f1c640949
amooryjubran <omarsalehjubran@gmail.com>
2023-04-30 13:26:21 -0400

    NextAuth, Prisma, Mongo Setups

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,10 @@
 import { useCallback, useState } from "react";
 import Input from "@/components/Input";
-
+import axios from "axios";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 const Auth = () => {
+  const router = useRouter();
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
@@ -12,6 +15,33 @@ const Auth = () => {
       currnetVarient === "login" ? "register" : "login"
     );
   }, []);
+
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: "/",
+      });
+      router.push("/profiles");
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        name,
+        password,
+      });
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -49,7 +79,10 @@ const Auth = () => {
                 value={password}
               />
             </div>
-            <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+            <button
+              onClick={varient === "login" ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
               {varient == "login" ? "Login" : "Sign Up"}
             </button>
             <p className="text-neutral-500 mt-12">
